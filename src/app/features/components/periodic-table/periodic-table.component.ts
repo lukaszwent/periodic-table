6d3fe06e3398d3c2700c34be5dd8005829523a68
Lukasz Went <zakzyne@gmail.com>
2024-09-14 00:52:31 +0200

    PT-4 Add query to filter periodic-table

--- src/app/features/components/periodic-table/periodic-table.component.ts
+++ src/app/features/components/periodic-table/periodic-table.component.ts
@@ -8,11 +8,25 @@ import { CommonModule } from '@angular/common'
 import { MatCard, MatCardContent } from '@angular/material/card'
 import { MatDialog } from '@angular/material/dialog'
 import { PeriodicEditDialogComponent } from '../periodic-edit-dialog/periodic-edit-dialog.component'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { debounceTime, Subject } from 'rxjs'
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule } from '@angular/forms'
+import { NoRecordsFoundComponent } from '../../../shared/components/no-records-found/no-records-found.component'
 
 @Component({
     selector: 'pt-periodic-table',
     standalone: true,
-    imports: [MatTableModule, CommonModule, MatCard, MatCardContent],
+    imports: [
+        MatTableModule,
+        CommonModule,
+        MatCard,
+        MatCardContent,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        NoRecordsFoundComponent,
+    ],
     templateUrl: './periodic-table.component.html',
     styleUrl: './periodic-table.component.css',
 })
@@ -24,12 +38,36 @@ export class PeriodicTableComponent implements OnInit {
 
     periodicData: WritableSignal<PeriodicElement[]> = signal([])
 
+    filteredPeriodicData: WritableSignal<PeriodicElement[]> = signal([])
+
     displayedColumns = ['position', 'name', 'weight', 'symbol']
+
+    query: string = ''
+
+    queryChange: Subject<string> = new Subject<string>()
 
     ngOnInit(): void {
         this.route.snapshot.data['periodicData'].subscribe((data: PeriodicElement[]) => {
             this.periodicData.set(data)
+            this.filteredPeriodicData.set(data)
         })
+
+        this.queryChange.pipe(debounceTime(2000)).subscribe((search) => {
+            this.filteredPeriodicData.set(
+                this.periodicData().filter(
+                    (val) =>
+                        val.position.toString().toLowerCase().indexOf(search.trim().toLowerCase()) !== -1 ||
+                        val.name.toLowerCase().indexOf(search.trim().toLowerCase()) !== -1 ||
+                        val.symbol.toLowerCase().indexOf(search.trim().toLowerCase()) !== -1 ||
+                        val.weight.toString().toLowerCase().indexOf(search.trim().toLowerCase()) !== -1
+                )
+            )
+        })
+    }
+
+    onQueryChange(value: string): void {
+        this.queryChange.next(value)
+        this.query = value
     }
 
     editPeriodic(periodic: PeriodicElement): void {
