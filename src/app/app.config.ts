8ca4ad14daf7e363d026a12360698049d64f6509
Lukasz Went <zakzyne@gmail.com>
2024-09-17 08:52:47 +0200

    PT-6 Add RxAngular to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
 import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideRxStateConfig } from '@rx-angular/state'
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations()],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideAnimations(),
+        provideRxStateConfig(),
+    ],
 }
--- src/app/features/resolvers/periodic-data.resolver.ts
+++ src/app/features/resolvers/periodic-data.resolver.ts
@@ -1,34 +1,29 @@
-import {
-  ActivatedRouteSnapshot,
-  ResolveFn,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
-import { PeriodicElement } from '../../types/periodic/periodic';
-import { inject } from '@angular/core';
-import { PeriodicDataService } from '../../apis/periodic-data.service';
-import { EMPTY, map, of, Observable } from 'rxjs';
-import { LoadingService } from '../../shared/services/loading.service';
+import { ActivatedRouteSnapshot, ResolveFn, Router, RouterStateSnapshot } from '@angular/router'
+import { PeriodicElement } from '../../types/periodic/periodic'
+import { inject } from '@angular/core'
+import { PeriodicDataService } from '../../apis/periodic-data.service'
+import { EMPTY, map, of, Observable } from 'rxjs'
+import { LoadingService } from '../../shared/services/loading.service'
 
 export const PeriodicDataResolver: ResolveFn<Observable<PeriodicElement[]>> = (
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
 ) => {
-  const router = inject(Router);
-  const loadingService = inject(LoadingService);
+    const router = inject(Router)
+    const loadingService = inject(LoadingService)
 
-  loadingService.loadingOn();
-  return inject(PeriodicDataService)
-    .getPeriodicTableData()
-    .pipe(
-      map((data) => {
-        if (data) {
-          loadingService.loadingOff();
-          return of(data);
-        }
+    loadingService.loadingOn()
+    return inject(PeriodicDataService)
+        .getPeriodicTableData()
+        .pipe(
+            map((data) => {
+                if (data) {
+                    loadingService.loadingOff()
+                    return of(data)
+                }
 
-        router.navigate(['error-page']);
-        return EMPTY;
-      })
-    );
-};
+                router.navigate(['error-page'])
+                return EMPTY
+            })
+        )
+}
